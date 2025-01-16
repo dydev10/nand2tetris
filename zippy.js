6f6003b5b1160af915a185d48f0bfd368775ed2e
DY Dev <dydev420@gmail.com>
2025-01-16 20:08:07 +0530

    rename constant for corse dir

--- zippy.js
+++ zippy.js
@@ -1,16 +1,16 @@
 import fs from 'fs';
 import archiver from 'archiver';
 
-const TEST_DIR = 'course';
-const directoryFiles = fs.readdirSync(`./${TEST_DIR}`);
+const COURSE_DIR = 'course';
+const directoryFiles = fs.readdirSync(`./${COURSE_DIR}`);
 
 const zipProjectFolder = (project) => {
   return new Promise((resolve, reject) => {
     const archive = archiver('zip', {
       zlib: { level: 9 },
     });
-    const outputZip = fs.createWriteStream(`./${TEST_DIR}/${project}.zip`);
-    const projectFiles = fs.readdirSync(`./${TEST_DIR}/${project}`);
+    const outputZip = fs.createWriteStream(`./${COURSE_DIR}/${project}.zip`);
+    const projectFiles = fs.readdirSync(`./${COURSE_DIR}/${project}`);
     archive.on('close', () => {
       console.log('Zip files created for:', project, projectFiles);
       resolve();
@@ -20,14 +20,14 @@ const zipProjectFolder = (project) => {
       reject();
     });
     archive.pipe(outputZip);
-    archive.directory(`./${TEST_DIR}/${project}/`, false);
+    archive.directory(`./${COURSE_DIR}/${project}/`, false);
     archive.finalize();
   });
 };
 
 const createZip = async () => {
   await Promise.all(directoryFiles.map((filename) => {
-    const fileStats = fs.statSync(`./${TEST_DIR}/${filename}`);
+    const fileStats = fs.statSync(`./${COURSE_DIR}/${filename}`);
     if (fileStats.isDirectory()) {
       return zipProjectFolder(filename);
     } else return null;
